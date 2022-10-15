c52d17a889ae6d246a76a841169b2fea69731080
filispeen <72784386+filispeen@users.noreply.github.com>
2022-10-15 15:27:21 -0400

    Update rtl_install.sh

--- rtl_install.sh
+++ rtl_install.sh
@@ -1,7 +1,7 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
-echo 'Таймер запуститься через 10 секунд запиши команду (sudo bash rtlp2.sh)'
+echo 'Таймер запуститься через 10 секунд запиши команду (sudo bash rtlp3.sh)'
 sleep 10
 clear
 echo Перезагрузка произойдёт через 10 секунд
@@ -37,4 +37,5 @@ clear
 echo Перезагрузка произойдёт через 0 секунд
 sleep 1
 clear
+wget https://raw.githubusercontent.com/filispeen/rtl8188eus-monitor-mode-driver-installer/main/rtlp3.sh
 reboot
